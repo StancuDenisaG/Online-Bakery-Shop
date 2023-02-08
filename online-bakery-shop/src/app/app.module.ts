d0a908ec6856ce9dae7e8ad45bb7cf642f12a183
Stancu Denisa <denisastancu354@gmail.com>
2023-02-08 13:46:16 +0200

    added header and home page

---
+++ online-bakery-shop/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire/compat';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule
+    
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ online-bakery-shop/src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'online-bakery-shop-b731e',
+    appId: '1:918608470155:web:9c2a425988bfb47e05b48e',
+    storageBucket: 'online-bakery-shop-b731e.appspot.com',
+    apiKey: 'AIzaSyCr2nixYu9tig7RZHdmBCYO1a7I9u6Rj5U',
+    authDomain: 'online-bakery-shop-b731e.firebaseapp.com',
+    messagingSenderId: '918608470155',
+  },};
